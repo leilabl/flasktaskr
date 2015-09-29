f7dda357fa52e6dce4b61d0569f6c00330f74014
leilabl <leilaloezer@gmail.com>
2015-09-29 19:28:18 -0400

    Basic app functionality

---
+++ project/_config.py
@@ -0,0 +1,14 @@
+import os
+import random
+
+# grab the folder where this script lives
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+WTF_CSRF_ENABLED = True
+SECRET_KEY = os.urandom(16)
+
+# define the full path for the database
+DATABASE_PATH = os.path.join(basedir, DATABASE)
