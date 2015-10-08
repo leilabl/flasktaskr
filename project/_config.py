d60b6d78b1dbba6ece0f7249083014a61b2d71f3
leilabl <leilaloezer@gmail.com>
2015-10-08 00:30:55 -0400

    Replaced mySQL database code with equivalent SQLAlchemy and recreated the database

--- project/_config.py
+++ project/_config.py
@@ -1,5 +1,6 @@
 import os
 import random
+#import pdb; pdb.set_trace()
 
 # grab the folder where this script lives
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -9,6 +10,9 @@ USERNAME = 'admin'
 PASSWORD = 'admin'
 WTF_CSRF_ENABLED = True
 SECRET_KEY = os.urandom(16)
-
+#import pdb; pdb.set_trace()
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# database uri
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
