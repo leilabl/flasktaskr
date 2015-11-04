e5c803aa721873f53dc2a68ec31e16cae31687de
leilabl <leilaloezer@gmail.com>
2015-11-04 18:40:26 -0500

    test

--- project/run.py
+++ project/run.py
@@ -1,7 +1,12 @@
 # project/run.py
 
+#for local serving
+# from views import app
+# app.run(debug=True)
+
+#for heroku deployment
 import os
-from views import app
+from project import app
 
 post = int(os.environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port)
